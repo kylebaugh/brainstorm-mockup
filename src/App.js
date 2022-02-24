d98534a8f3367b5b0e6fbd10f212474ca1fb67eb
kylebaugh <kylebaugh1@gmail.com>
2022-02-24 14:53:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,321 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class='header'>
+        <section class='headLeft'>
+          <img src='' alt='logo  '/>
+          BrainStorm
+        </section>
+        <section class='headMid'>
+          <section class='dropDown'>Solutions</section>
+          <section class='dropDown'>Products</section>
+          <section class='dropDown'>Partners</section>
+          <section class='dropDown'>Resources</section>
+          <section class='dropDown'>Company</section>
+        </section>
+        <section class='headRight'>
+          <section class='login'>Login</section>
+          <section class='demo'>Demo</section>
+        </section>
       </header>
+      
+        <section class='headerFoot'></section>
+
+      <section class='popup'>
+        <img src='' alt='logo'/>
+        <section>
+          Have any questions? I'm here to help!
+        </section>
+        <img src='' alt='chatBubble'/>
+
+      </section>
+
+      <div class='hero'>
+        <section class='heroLeft'>
+          <section class='hl1'>
+            Better with BrainStorm
+            <section class='hlUnderline'></section>
+          </section>
+          <section class='hl2'>
+            Simplifying<br/>
+            software<br/>
+            adoption for<br/>
+            everyone
+          </section>
+          <section class='hl3'>
+            BrainStorm QuickHelp uses a human approach
+            <br/>
+            that makes learning new software a snap.
+          </section>
+          <section class='hl4'>
+            <section class='hl4Left'>Get Started</section>
+            <section class='hl4Right'>Learn more</section>
+          </section>
+        </section>
+        <section class='heroRight'>
+          <img class='thumbPic' src='https://www.brainstorminc.com/hs-fs/hubfs/Hero.png?width=1375&height=945&name=Hero.png' alt='thumb pic'/>
+        </section>
+      </div>
+
+
+      <div class='trust'>
+        <section class='trust1'>
+          You'll be in good company
+          <section class='trustUnderline'></section>
+        </section>
+        <section class='trust2'>
+          Trusted by the best
+        </section>
+        <section class='trust3'>
+          <img class='t3Icon' src='https://www.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/Clorox%20Logo.svg' alt='clorox'/>
+          <img class='t3Icon' src='https://www.brainstorminc.com/hubfs/_BrainStorm_May2021/_logos/Pepsico_logo.svg' alt='pepsico'/>
+          <img class='t3Icon' src='https://www.brainstorminc.com/hubfs/_BrainStorm_May2021/_logos/Merck_logo.svg' alt='merck'/>
+          <img class='t3Icon' src='https://www.brainstorminc.com/hubfs/alliance-data-systems%20BW%20Logo.svg' alt='alliance'/>
+        </section>
+        <section class='trust4'>
+          <img class='naperville' src='https://www.brainstorminc.com/hubfs/New%20Website%20Images/Customer%20Story%20Image/Ciry_of_Naperville.svg' alt='naperville'/>
+          <p class='t4Quote'>“We first looked at BrainStorm as a short-term training solution, but as we continued to work with them, it became clear that they would become part of our long-term strategy.”</p>
+          <p class='t4Cite'>Russell Rogers – IT Project Manager, City of Naperville</p>
+        </section>
+      </div>
+
+
+      <div class='proofNumbers'>
+        <section class='proof1'>
+          Proof in numbers
+          <section class='proof1Underline'></section>
+        </section>
+        <section class='proof2'>
+          User adoption solutions
+          <br/>
+          with real results
+        </section>
+        <section class='proof3'>
+          45% of Fortune 100 companies have worked with BrainStorm to get the most out of their enterprise software.
+        </section>
+        <section class='proof4'>
+          <section class='p4l'>
+            <img class='p4Image' src='https://info.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/Stop%20losing%20millions.png' alt='arch'/>
+            <p class='p4Reason'>Stop losing millions</p>
+            <p class='p4Blurb'>The average enterprise wastes $7.4 million each year on software that is underused by their employees. Effective adoption and training have never been more important to an organization's bottom line.</p>
+            <p class='learnMore'>Learn more </p>
+          </section>
+          <section class='p4m'>
+            <img class='p4Image' src='https://info.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/Forget%20duplicate%20functionality.png' alt='wedge'/>
+            <p class='p4Reason'>Confidence against threats</p>
+            <p class='p4Blurb'>How secure are your end users? With BrainStorm, 98% of users report feeling better equipped to protect their organization from security threats.</p>
+            <p class='learnMore'>Learn more </p>
+          </section>
+          <section class='p4r'>
+            <img class='p4Image' src='https://info.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/Forget%20duplicate%20functionality.png' alt='plane'/>
+            <p class='p4Reason'>See adoption soar</p>
+            <p class='p4Blurb'>BrainStorm customers see adoption of Microsoft Teams soar by as much as 400% in less than 6 months.</p>
+            <p class='learnMore'>Learn more </p>
+          </section>
+        </section>
+      </div>
+
+
+      <div class='seeChange'>
+        <section class='seeChange1'>
+        See real change in user adoption
+        <section class='seeChange1Underline'></section>
+        </section>
+        <section class='seeChange2'>
+          Better engagement, better adoption
+        </section>
+        <section class='seeChange3'>
+          Nobody is unteachable. BrainStorm’s empowering and engaging solutions make your users want to learn, increasing usage and bringing tangible ROI.
+        </section>
+      </div>
+
+      
+      <div class='quickHelp'>
+        <section class='qhl'>
+          <h2>BrainStorm QuickHelpTM</h2>
+          <p>
+            Get the real user adoption change you need. BrainStorm QuickHelp™ moves beyond change management software to transform organizations, empower users, and activate cultural learning shifts.
+          </p>
+          <p class='learnMore'>Learn more </p>
+        </section>
+        <section class='qhr'>
+          <img class='qhrImage' src='https://info.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/BrainStorm%20QuickHelp%20Image.png' alt='kickback'/>
+        </section>
+      </div>
+
+
+      <div class='threatDefense'>
+        <section class='trl'>
+          <img class='trlImage' src='https://info.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/BrainStorm%20Threat%20Defense%20Image.png' alt='fishbowl'/>
+        </section>
+      <section class='trr'>
+          <h2>BrainStorm Threat Defense</h2>
+          <p>
+            It’s time to take a new human-focused approach to security awareness. Traditional training lacks a crucial human element, but BrainStorm Threat Defense isn’t your traditional user adoption solution.          </p>
+          <p class='learnMore'>Learn more </p>
+        </section>
+      </div>
+
+
+      <div class='instructorTraining'>
+      <section class='itl'>
+          <h2>Instructor-led training</h2>
+          <p>
+            Get the best of both worlds with a strategic combination of hands-on instruction and practice exercises that will build user confidence and expertise.          </p>
+          <p class='learnMore'>Learn more </p>
+        </section>
+        <section class='itr'>
+          <img class='itrImage' src='https://info.brainstorminc.com/hubfs/New%20Website%20Images/Home%20Page/BrainStorm%20Instructor-%20Led%20Training%20Image.png' alt='clipboard'/>
+        </section>
+      </div>
+
+
+      <div class='wordOnStreet'>
+        <section class='wordTop'>Word on the Street</section>
+        <img class='carousel' src='https://raw.githubusercontent.com/kylebaugh/portfolio/main/src/assets/pictures/carousel.png' alt='carousel'/>
+      </div>
+
+
+      <div class='suiteForChange'>
+        <section class='sfc1'>A suite for change</section>
+        <section class='sfc2'>Make tech adoption actually work</section>
+        <section class='sfc3'>It’s all in the approach. See how developing a personalized change management strategy can transform your teams.</section>
+        <section class='sfc4'>
+          <section class='sfc1Carousel'></section>  
+        </section>
+      </div>
+
+
+      <div class='resources'>
+        <section class='resources1'>Your BrainStorm resources</section>
+        <section class='resources2'>Learning about learning</section>
+        <section class='resources3'>Get insights into best practices, methods, and philosophies, and discover an approach that can help your users actually use their software.</section>
+        <section class='resources4'>
+          <section class='r4l'>
+            {/* backgroun Image */}
+            <section class='r4Box'>
+              <section class='r4BoxTop'>
+                <img src='' alt='doc icon'/>
+              </section>
+              <h3 class='r4BoxTop'></h3>
+              <p class='r4BoxTop'></p>
+            </section>
+          </section>
+          <section class='r4r'>
+            <section class='r4Box'>
+              <section class='r4BoxTop'>
+                <img src='' alt='doc icon'/>
+              </section>
+              <h3 class='r4BoxTop'></h3>
+              <p class='r4BoxTop'></p>
+            </section>
+          </section>
+        </section>
+      </div>
+
+
+      <div class='dontMiss'>
+        <h1>Don't miss a thing</h1>
+        <p></p>
+        <section>
+          <input placeholder='Email*'/>
+          <button>Subscribe</button>
+        </section>
+      </div>
+
+
+      <div class='scheduleDemo'>
+        <section class='demoLeft'>
+          See why change is everything
+        </section>
+        <section class='demoRight'>
+          <section class='inputs'>
+            <section class='drl'>
+              <input class='drInput' placeholder='First name*'/>
+              <input class='drInput' placeholder='Work title*'/>
+              <input class='drInput' placeholder='Phone number*'/>
+            </section>
+            <section class='drr'>
+              <input class='drInput' placeholder='Last name*'/>
+              <input class='drInput' placeholder='Work email*'/>
+              <input class='drInput' placeholder='Company*'/>
+            </section>
+          </section>
+          <button class='demoButton'>Schedule a demo</button>
+        </section>
+      </div>
+
+
+      <footer class='footer'>
+        <section class='footerTop'>
+          <section class='fT1'>
+            <p class='footHead'>
+
+            Solutions
+            </p>
+            <br/>
+              <p class='footerList'>Slack</p>
+              <p class='footerList'>Webex</p>
+              <p class='footerList'>Zoom</p>
+              <p class='footerList'>Threat Defense</p>
+              <p class='footerList'>Google Workspace</p>
+              <p class='footerList'>Microsoft 365</p>
+              <p class='footerList'>Smartsheet</p>
+              <p class='footerList'>Threat Defense</p>
+              <p class='footerList'>Box</p>
+              <p class='footerList'>Dropbox</p>
+          </section>
+          <section class='fT2'>
+          <p class='footHead'>
+            Products
+          </p>
+            <br/>
+              <p class='footerList'>QuickHelp</p>
+              <p class='footerList'>Instructor-led Training</p>
+            <br/>
+            <p class='footHead'>
+              Partners
+            </p>
+          </section>
+          <section class='fT3'>
+            <p class='footHead'>
+              Resources
+            </p>
+            <br/>
+              <p class='footerList'>All resources</p>
+              <p class='footerList'>Blog</p>
+              <p class='footerList'>Webinars</p>
+          </section>
+          <section class='fT4'>
+            Company
+            <br/>
+              <p class='footerList'>About</p>
+              <p class='footerList'>Careers</p>
+              <p class='footerList'>Contact Us</p>
+          </section>
+        </section>
+        <section class='footerLine'></section>
+
+        <section class='footerBottom'>
+          <section class='fbl'>
+            Copyright C2022 BrainStorm, Inc All rights reserved
+          </section>
+          <section class='fbm'>
+            <section>Legal | Privacy Policy | Terms and Conditions</section>
+             {/* | 
+            <section> Privacy Policy </section>
+             | 
+            <section> Terms and Conditions</section> */}
+          </section>
+          <section class='fbr'>
+            <img src='' alt='LinkedIN'/>
+            <img src='' alt='Twitter'/>
+            <img src='' alt='Facebook'/>
+            <img src='' alt='Instagram'/>
+          </section>
+        </section>
+      </footer>
+
     </div>
   );
 }
